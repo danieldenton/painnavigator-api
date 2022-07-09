6603c1b91fef8a32941d94574ddaf1144938ddf7
stephenavocado <stephenwalter@chicagobooth.edu>
2022-07-09 18:44:46 +0000

    fixed mailer template

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,10 +1,11 @@
 class MyMailer < ApplicationMailer   
   helper :application
+  layout 'mailer'
 
-  def contact_us(body, user)
-    reply_to = "#{user.first_name} #{user.last_name} <#{user.email}>"
-    @body = body
-    mail(to: "stephenwalter@chicagobooth.edu", subject: "New message from PainNavigator user", reply_to: reply_to)
+  def contact_us
+    #reply_to = "#{user.first_name} #{user.last_name} <#{user.email}>"
+    #@body = "body"
+    mail(to: "stephenwalter@chicagobooth.edu", subject: "New message from PainNavigator user")
   end
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'https://3000-stephenavoc-painnavigat-opr918elymm.ws-us53.gitpod.io/',
+    :domain => 'https://3000-stephenavoc-painnavigat-opr918elymm.ws-us53.gitpod.io',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
