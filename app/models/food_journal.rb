c66e857c39b45939a884b7946eacebea39cff93f
stephenavocado <stephenwalter@chicagobooth.edu>
2022-07-21 18:44:10 +0000

    fixed food journal date_time

--- app/models/food_journal.rb
+++ app/models/food_journal.rb
@@ -1,6 +1,7 @@
 class FoodJournal < ApplicationRecord
   belongs_to :user
-  after_create :set_date_time
+  #after_create :set_date_time
+  after_validation -> { self.date_time_value = Time.now.to_f * 1000 }, on: :create
 
   def set_date_time
     self.date_time_value = self.created_at.to_f * 1000
