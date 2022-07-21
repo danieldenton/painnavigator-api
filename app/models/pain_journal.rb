41e60f4cabf16680c0a9c54c3896427a1ec83d1b
stephenavocado <stephenwalter@chicagobooth.edu>
2022-07-21 17:47:09 +0000

    removed validation date

--- app/models/pain_journal.rb
+++ app/models/pain_journal.rb
@@ -1,9 +1,8 @@
 class PainJournal < ApplicationRecord
   belongs_to :user
-  #after_create :set_date_time
-  after_validation -> { self.date_time_value = Time.now }, on: :create
+  after_create :set_date_time
 
-  #def set_date_time
-    #self.date_time_value = self.created_at.to_f * 1000
-  #end
+  def set_date_time
+    self.date_time_value = self.created_at.to_f * 1000
+  end
 end
--- app/models/smart_goal.rb
+++ app/models/smart_goal.rb
@@ -2,8 +2,7 @@ class SmartGoal < ApplicationRecord
   belongs_to :user
   has_many  :smart_goal_updates, dependent: :destroy
   enum status: [:active, :finished]
-  #after_create :set_date_time
-  after_validation -> { self.date_time_value = Time.now }, on: :create
+  after_create :set_date_time
 
   def set_date_time
     self.date_time_value = self.created_at.to_f * 1000
