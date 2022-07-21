4ec8213e1895e98a51b8591bc2cfd5865870e66c
stephenavocado <stephenwalter@chicagobooth.edu>
2022-07-21 17:29:59 +0000

    date_time_value post create validation

--- app/models/pain_journal.rb
+++ app/models/pain_journal.rb
@@ -1,8 +1,9 @@
 class PainJournal < ApplicationRecord
   belongs_to :user
-  after_create :set_date_time
+  #after_create :set_date_time
+  after_validation -> { self.date_time_value = Time.now }, on: :create
 
-  def set_date_time
-    self.date_time_value = self.created_at.to_f * 1000
-  end
+  #def set_date_time
+    #self.date_time_value = self.created_at.to_f * 1000
+  #end
 end
--- app/models/smart_goal.rb
+++ app/models/smart_goal.rb
@@ -2,7 +2,8 @@ class SmartGoal < ApplicationRecord
   belongs_to :user
   has_many  :smart_goal_updates, dependent: :destroy
   enum status: [:active, :finished]
-  after_create :set_date_time
+  #after_create :set_date_time
+  after_validation -> { self.date_time_value = Time.now }, on: :create
 
   def set_date_time
     self.date_time_value = self.created_at.to_f * 1000
