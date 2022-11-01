802b0df99f7d4e35395818da4d8655eda6dbb76b
stephenavocado <stephenwalter@chicagobooth.edu>
2022-11-01 02:27:35 +0000

    updated Provider serializer to users_count from user_count

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,4 +1,4 @@
 class ProviderSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :code, :user_count
+  attributes :id, :name, :code, :users_count
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#admin'
   get '/wellness' => 'application#wellness'
   post '/reply_to_user' => 'application#reply_to_user'
   
