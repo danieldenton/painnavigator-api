f0ce4fcaa4a84931ec6f768a213e0abdb4c4f721
stephenavocado <stephenwalter@chicagobooth.edu>
2022-12-10 20:02:47 +0000

    add timestamp method to message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+  require "date"
   belongs_to :sender, class_name: "User"
   belongs_to :recipient, class_name: "User"
   after_create :set_date_time
@@ -7,5 +8,9 @@ class Message < ApplicationRecord
 
   def set_date_time
     self.date_time_value = self.created_at.to_f * 1000
+  end
+
+  def timestamp
+    return created_at.strftime("%A %b %e, %l:%M %P")
   end
 end
