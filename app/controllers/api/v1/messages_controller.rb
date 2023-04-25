2956a4e18c7ee26fba459a2ead305026b21c938e
Daniel Denton <danielmdenton@gmail.com>
2023-04-25 04:42:12 +0000

    maybe committing and pushing will help this

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class MessagesController < ApplicationController
+      require 'exponent-server-sdk'
       before_action :find_user, only: [:get_conversation, :mark_conversation_read]
       
       def get_conversation
@@ -45,25 +46,25 @@ module Api
         message = Message.new(sender_id: @sender.id, recipient_id: @recipient.id, body: body)
 
         if message.save
+          puts "MESSAGE SAVING"
           if @sender.role == "standard"
             @sender.update(has_unreplied_message: true)
           elsif @sender.role == "wellness_coach" || @sender.role == "admin"
-
             expo_push_token = "ExponentPushToken[pl4gsnDTGSWaqjhi2D_4hj]"
             if expo_push_token.present?
-              message = {
+              messages = {
                 to: expo_push_token,
-                sound: "default",
                 body: "You have a new message from your wellness coach"
               }
               client = Exponent::Push::Client.new
-              handler = client.publish(message)
+              handler = client.publish(messages)
               client.verify_deliveries(handler.receipt_ids)
             end
             @recipient.update(has_unreplied_message: false)
           end
           render json: MessageSerializer.new(message).serializable_hash.to_json
         else 
+          puts "creating an error"
           render json: { error: message.errors.messages }, status: 422
         end
       end
