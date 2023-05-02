fcf0f98910fe9d0106083d202384be97d55e4433
Daniel Denton <danielmdenton@gmail.com>
2023-05-02 18:34:53 +0000

    attempt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_02_173829) do
+ActiveRecord::Schema.define(version: 2023_05_02_183328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -206,7 +206,7 @@ ActiveRecord::Schema.define(version: 2023_05_02_173829) do
     t.integer "wellness_coach_id"
     t.boolean "has_unreplied_message", default: false
     t.string "expo_push_token"
-    t.boolean "completed_program"
+    t.boolean "completed_program", default: false
   end
 
   create_table "wellness_coaches", force: :cascade do |t|
