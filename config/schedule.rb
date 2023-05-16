fa3b17b2ab70f34549c139fc1adb21da68746d08
Daniel Denton <danielmdenton@gmail.com>
2023-05-16 17:48:53 +0000

    central time zone set/

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,12 @@
 
 # Learn more: http://github.com/javan/whenever
 
+set :environment, 'production'
+set :output, 'log/cron.log'
+set :job_template, "/bin/bash -l -c ':job'"
+
 every '0 12 * * 1-5' do
+  tz 'Central Time (US & Canada)'
   rake 'push_notifications:send'
 end
 
