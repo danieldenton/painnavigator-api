02f56d09bf56c6f7d8d2283ba25ba0ed8aca616a
Daniel Denton <danielmdenton@gmail.com>
2023-05-17 20:35:33 +0000

    default timezone setup in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module VanillaRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Set the default time zone for the application
+    config.time_zone = "Central Time (US & Canada)"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
