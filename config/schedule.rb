55132a5721bd33897fa3398d6acaffb5878a22b8
Daniel Denton <danielmdenton@gmail.com>
2023-05-19 17:38:10 +0000

    time set to 12:45

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,6 +23,6 @@ set :environment, 'production,development'
 set :output, 'log/cron.log'
 set :job_template, "/bin/bash -l -c ':job'"
 
-every '30 17 * * 1-5' do
+every '45 17 * * 1-5' do
   runner 'PushNotificationsJob.perform'
 end
