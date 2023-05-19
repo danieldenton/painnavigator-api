a28f01fad06d6b4b22beb84828f9ccc5384639d2
Daniel Denton <danielmdenton@gmail.com>
2023-05-19 18:20:57 +0000

    Revert to commit c2b6baebbf1542e0a8357e0180b2f43a237fe21b

--- config/initializers/production_key.rb
+++
@@ -1,7 +0,0 @@
-production_key_path = Rails.root.join('config', 'credentials', 'production.key')
-production_key = File.read(production_key_path).strip
-
-# Configure your application to use the production key
-PainNavigatorMobileApi.configure do |config|
-  config.production_key = production_key
-end
