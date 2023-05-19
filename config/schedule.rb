0081c121b814d5378ea3baf366f68e5374e358e0
Daniel Denton <danielmdenton@gmail.com>
2023-05-19 20:02:08 +0000

    gonna merge

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,6 +23,6 @@ set :environment, 'production,development'
 set :output, 'log/cron.log'
 set :job_template, "/bin/bash -l -c ':job'"
 
-every '30 19 * * 1-5' do
+every '34 19 * * 1-5' do
   runner 'PushNotificationsJob.perform'
 end
