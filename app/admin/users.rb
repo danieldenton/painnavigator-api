f2156e7b76b63741433a9fcff7fb8105f7d33087
Daniel Denton <danielmdenton@gmail.com>
2023-05-26 19:18:29 +0000

    active admin dash updated movement unit progress and education progress

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :role, :uid, :first_name, :last_name, :gender, :commitment, :onboard_status, :profile_status, :phone, :starting_pain_duration, :activity_level, :pace, :starting_pain_score, :enjoyment_of_life, :activity_interference, :hopes_to_achieve, :dob, :email, :mood_journals_count, :food_journals_count, :education_modules_count, :movement_modules_count, :bookmarks_count, :smart_goals_count, :provider_id, :wellness_coach_id, :completed_program
+  permit_params :role, :uid, :first_name, :last_name, :gender, :commitment, :onboard_status, :profile_status, :phone, :starting_pain_duration, :activity_level, :pace, :starting_pain_score, :enjoyment_of_life, :activity_interference, :hopes_to_achieve, :dob, :email, :mood_journals_count, :food_journals_count, :education_progress, :movement_progress, :bookmarks_count, :smart_goals_count, :provider_id, :wellness_coach_id, :completed_program
  
   index do
     id_column
@@ -28,8 +28,28 @@ ActiveAdmin.register User do
     column :email
     column :mood_journals_count
     column :food_journals_count
-    column :education_modules_count
-    column :movement_modules_count
+    column :education_progress, sortable: :education_progress do |user|
+      progress = user.education_progress["progress"]
+      last_completed_date = user.education_progress["last_completed_date"]
+
+    if last_completed_date.nil?
+      "#{progress}"
+    else
+      formatted_date = Time.at(last_completed_date / 1000).strftime('%m-%d-%y')
+      "#{progress} on #{formatted_date}"
+    end
+    end
+    column :movement_progress, sortable: :movement_progress do |user|
+      progress = user.movement_progress["progress"]
+      last_completed_date = user.movement_progress["last_completed_date"]
+
+    if last_completed_date.nil?
+      "#{progress}"
+    else
+      formatted_date = Time.at(last_completed_date / 1000).strftime('%m-%d-%y')
+      "#{progress} on #{formatted_date}"
+    end
+    end
     column :smart_goals_count
     column :provider_id
     column :completed_program
