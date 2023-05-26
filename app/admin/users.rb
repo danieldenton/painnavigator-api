087eda04f4a7dc780f1d816b71ace67ff68a79de
Daniel Denton <danielmdenton@gmail.com>
2023-05-26 19:28:08 +0000

    active admin dash updated movement unit progress and education progress

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -33,10 +33,10 @@ ActiveAdmin.register User do
       last_completed_date = user.education_progress["last_completed_date"]
 
     if last_completed_date.nil?
-      "#{progress}"
+      "None"
     else
       formatted_date = Time.at(last_completed_date / 1000).strftime('%m-%d-%y')
-      "#{progress} on #{formatted_date}"
+      "Unit #{progress} on #{formatted_date}"
     end
     end
     column :movement_progress, sortable: :movement_progress do |user|
@@ -44,7 +44,7 @@ ActiveAdmin.register User do
       last_completed_date = user.movement_progress["last_completed_date"]
 
     if last_completed_date.nil?
-      "#{progress}"
+      "None"
     else
       formatted_date = Time.at(last_completed_date / 1000).strftime('%m-%d-%y')
       "#{progress} on #{formatted_date}"
