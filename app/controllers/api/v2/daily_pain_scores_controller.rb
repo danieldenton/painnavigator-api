e711f6e149a7d86f08fc882b1fa9aa7c206c7946
Daniel Denton <danielmdenton@gmail.com>
2023-06-06 16:32:51 +0000

    daily pain scores now found by uid

--- app/controllers/api/v2/daily_pain_scores_controller.rb
+++ app/controllers/api/v2/daily_pain_scores_controller.rb
@@ -1,20 +1,40 @@
 class Api::V2::DailyPainScoresController < ApplicationController
   before_action :find_user_by_uid, only: [:index, :create]
+  before_action :set_daily_pain_score, only: [:show, :update, :destroy]
 
+  # GET /api/v2/users/:user_id/daily_pain_scores
   def index
-    # Find daily pain scores for the user
-    daily_pain_scores = @user.daily_pain_scores
-    render json: daily_pain_scores
+    @daily_pain_scores = @user.daily_pain_scores
+    render json: @daily_pain_scores
   end
 
+  # GET /api/v2/users/:user_id/daily_pain_scores/:id
+  def show
+    render json: @daily_pain_score
+  end
+
+  # POST /api/v2/users/:user_id/daily_pain_scores
   def create
-    # Create a new daily pain score for the user
-    daily_pain_score = @user.daily_pain_scores.build(daily_pain_score_params)
-    if daily_pain_score.save
-      render json: daily_pain_score, status: :created
+    @daily_pain_score = @user.daily_pain_scores.new(daily_pain_score_params)
+    if @daily_pain_score.save
+      render json: @daily_pain_score, status: :created
     else
-      render json: { errors: daily_pain_score.errors.full_messages }, status: :unprocessable_entity
+      render json: @daily_pain_score.errors, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /api/v2/users/:user_id/daily_pain_scores/:id
+  def update
+    if @daily_pain_score.update(daily_pain_score_params)
+      render json: @daily_pain_score
+    else
+      render json: @daily_pain_score.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v2/users/:user_id/daily_pain_scores/:id
+  def destroy
+    @daily_pain_score.destroy
   end
 
   private
@@ -24,7 +44,11 @@ class Api::V2::DailyPainScoresController < ApplicationController
     render json: { error: 'User not found' }, status: :not_found unless @user
   end
 
+  def set_daily_pain_score
+    @daily_pain_score = @user.daily_pain_scores.find(params[:id])
+  end
+
   def daily_pain_score_params
-    params.require(:daily_pain_score).permit(:score)
+    params.require(:daily_pain_score).permit(:scores)
   end
 end
