9acd3268910fd322a9632953cb2af4ccf29f3789
Daniel Denton <danielmdenton@gmail.com>
2023-06-06 23:22:49 +0000

    delete and show removed from danilt_pain_score controller

--- app/controllers/api/v2/daily_pain_scores_controller.rb
+++ app/controllers/api/v2/daily_pain_scores_controller.rb
@@ -2,43 +2,6 @@ class Api::V2::DailyPainScoresController < ApplicationController
   before_action :find_user_by_uid, only: [:index, :create]
   before_action :set_daily_pain_score, only: [:show, :update, :destroy]
 
-  # GET /api/v2/users/:user_id/daily_pain_scores
-  def index
-    @daily_pain_scores = @user.daily_pain_scores
-    render json: @daily_pain_scores
-  end
-
-  # GET /api/v2/users/:user_id/daily_pain_scores/:id
-  def show
-    render json: @daily_pain_score
-  end
-
-  # POST /api/v2/users/:user_id/daily_pain_scores
-  def create
-    @daily_pain_score = @user.daily_pain_scores.new(daily_pain_score_params)
-    if @daily_pain_score.save
-      render json: @daily_pain_score, status: :created
-    else
-      render json: @daily_pain_score.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /api/v2/users/:user_id/daily_pain_scores/:id
-  def update
-    if @daily_pain_score.update(daily_pain_score_params)
-      render json: @daily_pain_score
-    else
-      render json: @daily_pain_score.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /api/v2/users/:user_id/daily_pain_scores/:id
-  def destroy
-    @daily_pain_score.destroy
-  end
-
-  private
-
   def find_user_by_uid
     @user = User.find_by(uid: params[:uid])
     render json: { error: 'User not found' }, status: :not_found unless @user
@@ -49,6 +12,30 @@ class Api::V2::DailyPainScoresController < ApplicationController
   end
 
   def daily_pain_score_params
-    params.require(:daily_pain_score).permit(:scores)
+    params.require(:daily_pain_score).permit(:score, :created_at, :updated_at)
   end
+
+  def index
+    @daily_pain_scores = @user.daily_pain_scores
+    render json: @daily_pain_scores
+  end
+
+  def create
+    @daily_pain_score = @user.daily_pain_scores.new(daily_pain_score_params)
+    if @daily_pain_score.save
+      render json: @daily_pain_score, status: :created
+    else
+      render json: @daily_pain_score.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @daily_pain_score.update(daily_pain_score_params)
+      render json: @daily_pain_score
+    else
+      render json: @daily_pain_score.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
 end
