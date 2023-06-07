50994225c114c449f8d85cc5fb5c4a208090d847
Daniel Denton <danielmdenton@gmail.com>
2023-06-07 21:06:35 +0000

    daily pain scores removed 2

--- app/models/daily_pain_score.rb
+++ app/models/daily_pain_score.rb
@@ -1,3 +1,7 @@
 class DailyPainScore < ApplicationRecord
   belongs_to :user
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> parent of ef565bc (all daily_pain_score removed)
--- app/serializers/daily_pain_score_serializer.rb
+++ app/serializers/daily_pain_score_serializer.rb
@@ -1,5 +1,13 @@
 class DailyPainScoreSerializer < ActiveModel::Serializer
+<<<<<<< HEAD
   attributes :id, :score, :created_at, :updated_at
 
   belongs_to :user
 end
+=======
+  attributes :id, :scores, :created_at, :updated_at
+  
+  belongs_to :user
+end
+
+>>>>>>> parent of ef565bc (all daily_pain_score removed)
