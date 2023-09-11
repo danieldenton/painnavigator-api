24b02de893520c7e220f552cac0818c6639890d8
Daniel Denton <danielmdenton@gmail.com>
2023-09-11 21:19:38 +0000

    movement apiV2 added

---
+++ app/controllers/api/v2/movement_module_completions_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class MovementModuleCompletionsController < ApplicationController
+      before_action :get_user
+
+      def index
+        # movement_modules = MovementModuleCompletion.all.order(:created_at).reverse
+        movement_modules = @user.movement_modules
+
+        render json: MovementModuleCompletionSerializer.new(movement_modules).serializable_hash.to_json
+      end
+
+      def create
+        movement_module = @user.movement_modules.new(movement_module_params)
+
+        if movement_module.save
+          render json: MovementModuleCompletionSerializer.new(movement_module).serializable_hash.to_json
+        else 
+          render json: { error: movement_module.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        movement_module = MovementModuleCompletion.find_by(id: params[:id])
+
+        if movement_module.update(movement_module_params)
+          render json: MovementModuleCompletionSerializer.new(movement_module).serializable_hash.to_json
+        else 
+          render json: { error: movement_module.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def movement_module_params
+        params.require(:movement_module)
+        .permit(:uid, :user_id, :status, :module_id)
+      end
+
+    end
+  end
+end
