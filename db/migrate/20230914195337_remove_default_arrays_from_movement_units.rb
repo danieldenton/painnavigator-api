134d8e5ef7a415532edb13828957b35e4ca5fea5
Daniel Denton <danielmdenton@gmail.com>
2023-09-14 19:55:50 +0000

    updated migration

--- db/migrate/20230914195337_remove_default_arrays_from_movement_units.rb
+++ db/migrate/20230914195337_remove_default_arrays_from_movement_units.rb
@@ -1,7 +1,7 @@
 class RemoveDefaultArraysFromMovementUnits < ActiveRecord::Migration[6.1]
   def change
-    change_column_default(:movement_units, :saved_movement_units, nil)
-    change_column_default(:movement_units, :skipped_movement_units, nil)
-    change_column_default(:movement_units, :completed_movement_units, nil)
+    change_column_default(:saved_movement_units, nil)
+    change_column_default(:skipped_movement_units, nil)
+    change_column_default(:completed_movement_units, nil)
   end
 end
