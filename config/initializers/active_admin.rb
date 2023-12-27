5893b04ef5548cadc8d8054ad4328b74bde28aae
Daniel Denton <danielmdenton@gmail.com>
2023-12-27 18:59:37 +0000

    deleted use webpacker from active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -331,5 +331,4 @@ ActiveAdmin.setup do |config|
   # By default, Active Admin uses Sprocket's asset pipeline.
   # You can switch to using Webpacker here.
   #
-  # config.use_webpacker = true
 end
