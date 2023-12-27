a6a416254a8df875e5a8ac4ad11d009768d3efc1
Daniel Denton <danielmdenton@gmail.com>
2023-12-27 20:55:23 +0000

    updated admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :role, :uid, :first_name, :last_name, :profile_status, :phone, :starting_pain_score, :enjoyment_of_life, :activity_interference, :hopes_to_achieve, :dob, :email, pain_journals: [], mood_journals: [], food_journals: [], :education_program, :education_progress, :movement_progress, :bookmarks_count, :smart_goals, :provider_id, :wellness_coach_id, :completed_program
+  permit_params :role, :uid, :first_name, :last_name, :profile_status, :phone, :starting_pain_score, :enjoyment_of_life, :activity_interference, :hopes_to_achieve, :dob, :email, pain_journals: [], mood_journals: [], food_journals: [], :education_program, :education_progress, :movement_progress, :bookmarks_count, smart_goals: [], :provider_id, :wellness_coach_id, :completed_program
  
   index do
     id_column
