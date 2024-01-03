7e3e8d8553c0c6641eaaf0c25007fc6176d50fee
Daniel Denton <danielmdenton@gmail.com>
2024-01-03 01:28:03 +0000

    namespace do added to dps admin test

--- app/admin/daily_pain_scores.rb
+++ app/admin/daily_pain_scores.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register DailyPainScore do
+ActiveAdmin.register DailyPainScore, namespace: :admin do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
