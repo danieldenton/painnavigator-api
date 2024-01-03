f77e29e58924ac795e6ea5c0e4beb2801a97aac4
Daniel Denton <danielmdenton@gmail.com>
2024-01-03 03:11:22 +0000

    testing new admin

--- app/admin/daily_pain_scores.rb
+++ app/admin/daily_pain_scores.rb
@@ -1,25 +1,24 @@
-ActiveAdmin.register DailyPainScore, as: "Check" do
+ActiveAdmin.register DailyPainScore do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :id, :user_id, :score 
-
+  permit_params :user_id, :score, :date_time_value
 
   index do
     id_column
-    column :id
     column :user_id
     column :score
+    column :date_time_value
     actions
   end
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :code, :users_count]
+  #   permitted = [:user_id, :score, :date_time_value]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
