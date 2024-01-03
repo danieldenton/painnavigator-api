324354c7f102897e36e18f62e36bef81b2af1c02
Daniel Denton <danielmdenton@gmail.com>
2024-01-03 03:00:44 +0000

    namespace check

--- app/admin/daily_pain_scores.rb
+++ app/admin/daily_pain_scores.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register DailyPainScore do as: "Check"
+ActiveAdmin.register DailyPainScore, as: "Check" 
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
