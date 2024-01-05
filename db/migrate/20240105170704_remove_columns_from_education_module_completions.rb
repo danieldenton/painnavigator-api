d66df6e2786b4feae68661f3f0651f78c35ba6c4
Daniel Denton <danielmdenton@gmail.com>
2024-01-05 19:55:29 +0000

    education_progress removed from remove_comlumn migration

--- db/migrate/20240105170704_remove_columns_from_education_module_completions.rb
+++ db/migrate/20240105170704_remove_columns_from_education_module_completions.rb
@@ -1,6 +1,5 @@
 class RemoveColumnsFromEducationModuleCompletions < ActiveRecord::Migration[6.1]
   def change
     remove_column :education_module_completions, :date_time_value, :float
-    remove_column :education_module_completions, :education_progress, :integer
   end
 end
