a6fe2a36b71eff0b363ad77e52244fa4a80b8d11
Daniel Denton <danielmdenton@gmail.com>
2024-01-05 17:13:19 +0000

    updated EducationModuleCompletion model. Removed date_time_value.

--- app/models/education_module_completion.rb
+++ app/models/education_module_completion.rb
@@ -12,11 +12,6 @@
 #
 class EducationModuleCompletion < ApplicationRecord
   belongs_to :user, counter_cache: :education_modules_count
-  after_create :set_date_time
   enum status: [:completed, :skipped]
-
-  def set_date_time
-    self.date_time_value = self.created_at.to_f * 1000
-  end
 
 end
