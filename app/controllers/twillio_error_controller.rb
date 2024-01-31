664ba64330880d9f142ea595ceda0bea80cac654
Daniel Denton <danielmdenton@gmail.com>
2024-01-31 02:34:51 +0000

    now its a post again

--- app/controllers/twillio_error_controller.rb
+++ app/controllers/twillio_error_controller.rb
@@ -1,8 +1,9 @@
 class TwillioErrorController < ApplicationController
+  skip_before_action :verify_authenticity_token # Skip CSRF protection for debugging purposes
 
   def handle
-    payload = params[:payload]
-    # Your logic to handle or inspect the payload
+    payload = request.raw_post
+    Rails.logger.debug "Webhook Payload: #{payload}"
     render plain: "Webhook Payload: #{payload}"
   end
 end
