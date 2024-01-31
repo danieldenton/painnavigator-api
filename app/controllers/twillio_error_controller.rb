27867d8cde27977c9732d92a4ddec0da49a11ce9
Daniel Denton <danielmdenton@gmail.com>
2024-01-31 02:23:40 +0000

    twillio_error is now a get route to test

--- app/controllers/twillio_error_controller.rb
+++ app/controllers/twillio_error_controller.rb
@@ -1,15 +1,10 @@
 class TwillioErrorController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:debug] 
 
-  def debug
-    payload = request.raw_post
-    Rails.logger.debug "Twilio Error Payload: #{payload}"
-
-    # You can also print to the console if you are running the server locally
-    puts "Twilio Error Payload: #{payload}"
-
-    # Respond with a success status
-    head :ok
+  def handle
+    payload = params[:payload]
+    # Your logic to handle or inspect the payload
+    render plain: "Webhook Payload: #{payload}"
+  end
   end
 
 end
