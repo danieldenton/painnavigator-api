55a047441360fbde673fd4547f5e088c994a3565
Daniel Denton <danielmdenton@gmail.com>
2024-02-01 03:04:14 +0000

    check

--- app/controllers/twillio_stop_controller.rb
+++ app/controllers/twillio_stop_controller.rb
@@ -6,16 +6,18 @@ class TwillioStopController < ApplicationController
     incoming_number = params[:From]
     incoming_body = params[:Body]
     user = User.find_by(phone: incoming_number)
+    name = user.first_name
 
-    Rails.logger.info "Incoming SMS from: #{incoming_number}: #{incoming_body}"
+    # Rails.logger.info "Incoming SMS from: #{incoming_number}: #{incoming_body}"
+    Rails.logger.info "#{user}"
 
-    if incoming_body.downcase == "stop"
+    if incoming_body.downcase.strip == "stop"
       user.update(opt_out_sms: true)
-      twiml = Twilio::TwiML::MessagingResponse.new do |response|
-        response.message(body: "You have successfully unsubscribed from PainNavigator's reminder SMS messages. If you wish to restart these messages simply reply 'START' to this number. If you need anything else at all please get in touch with your Wellness Coach through you PainNavigator app.")
-      end
+      # twiml = Twilio::TwiML::MessagingResponse.new do |response|
+      #   response.message(body: "You have successfully unsubscribed from PainNavigator's reminder SMS messages. If you wish to restart these messages simply reply 'START' to this number. If you need anything else at all please get in touch with your Wellness Coach through you PainNavigator app.")
+      # end
       
-    elsif incoming_body.downcase == "start"
+    elsif incoming_body.downcase.strip == "start"
       user.update(opt_out_sms: false)
       twiml = Twilio::TwiML::MessagingResponse.new do |response|
         response.message(body: "You have successfully subscribed to PainNavigator's reminder SMS messages!")
