a048056649b34b17a4734d8b7a23229e6796de99
Daniel Denton <danielmdenton@gmail.com>
2024-02-01 03:07:59 +0000

    final Twillio stop messaging working and posting

--- app/controllers/twillio_stop_controller.rb
+++ app/controllers/twillio_stop_controller.rb
@@ -6,26 +6,18 @@ class TwillioStopController < ApplicationController
     incoming_number = params[:From]
     incoming_body = params[:Body]
     user = User.find_by(phone: incoming_number)
-    name = user.first_name
 
-    # Rails.logger.info "Incoming SMS from: #{incoming_number}: #{incoming_body}"
-    Rails.logger.info "#{user}"
+    Rails.logger.info "Incoming SMS from: #{incoming_number}: #{incoming_body}"
 
     if incoming_body.downcase.strip == "stop"
       user.update(opt_out_sms: true)
-      # twiml = Twilio::TwiML::MessagingResponse.new do |response|
-      #   response.message(body: "You have successfully unsubscribed from PainNavigator's reminder SMS messages. If you wish to restart these messages simply reply 'START' to this number. If you need anything else at all please get in touch with your Wellness Coach through you PainNavigator app.")
-      # end
       
     elsif incoming_body.downcase.strip == "start"
       user.update(opt_out_sms: false)
-      twiml = Twilio::TwiML::MessagingResponse.new do |response|
-        response.message(body: "You have successfully subscribed to PainNavigator's reminder SMS messages!")
-      end
 
     else
       twiml = Twilio::TwiML::MessagingResponse.new do |response|
-        response.message(body: "Hello! This is PainNavigator's automated SMS messaging number. To get in touch with PainNavigator please message your Wellness Coach through the PainNavigator app! Otherwise to unsubscribe from these reminder text messages simply reply 'STOP' or to subscribe 'START'. Have a great day!")
+        response.message(body: "Hello! This is PainNavigator's automated SMS messaging number. To get in touch with PainNavigator please message your Wellness Coach through the PainNavigator app! Have a great day!")
       end
     end
 
