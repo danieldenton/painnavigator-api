84f6c0ea65740674f185bb1c6518ef6fbb6040a6
Daniel Denton <danielmdenton@gmail.com>
2024-02-01 01:23:18 +0000

    updated twillio post

--- app/controllers/twillio_stop_controller.rb
+++ app/controllers/twillio_stop_controller.rb
@@ -1,10 +1,23 @@
+require 'twilio-ruby'
+
 class TwillioStopController < ApplicationController
   skip_before_action :verify_authenticity_token # Skip CSRF protection for debugging purposes
 
   def handle
-    payload = request.raw_post
-    Rails.logger.debug "Webhook Payload: #{payload}"
-    render plain: "Webhook Payload: #{payload}"
+    # Access the incoming SMS body
+    incoming_body = params[:Body]
+
+    # Log the incoming SMS body
+    Rails.logger.info "Incoming SMS Body: #{incoming_body}"
+
+    # Your logic to handle the incoming SMS goes here
+
+    # Respond to Twilio with a simple message (optional)
+    twiml = Twilio::TwiML::MessagingResponse.new do |response|
+      response.message(body: "Hey!")
+    end
+
+    render xml: twiml.to_s
   end
 end
 
