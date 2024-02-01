89ed4c4c0816fe7226eb3e289dba80d5e4ee7840
Daniel Denton <danielmdenton@gmail.com>
2024-02-01 02:40:30 +0000

    twillio_stop controller should be working. Testing next.

--- app/controllers/twillio_stop_controller.rb
+++ app/controllers/twillio_stop_controller.rb
@@ -1,18 +1,30 @@
 require 'twilio-ruby'
 
-class TwillioStopController < ApplicationController=
+class TwillioStopController < ApplicationController
 
   def handle
+    incoming_number = params[:From]
     incoming_body = params[:Body]
+    user = User.find_by(phone: incoming_number)
 
-    # Log the incoming SMS body
-    Rails.logger.info "Incoming SMS Body: #{incoming_body}"
+    Rails.logger.info "Incoming SMS from: #{incoming_number}: #{incoming_body}"
 
-    # Your logic to handle the incoming SMS goes here
+    if incoming_body.donwcase == "stop"
+      user.update(opt_out_sms: true)
+      twiml = Twilio::TwiML::MessagingResponse.new do |response|
+        response.message(body: "You have successfully unsubscribed from PainNavigator's reminder SMS messages. If you wish to restart these messages simply reply 'START' to this number. If you need anything else at all please get in touch with your Wellness Coach through you PainNavigator app.")
+      end
+      
+    elsif incoming_body.donwcase == "start"
+      user.update(opt_out_sms: false)
+      twiml = Twilio::TwiML::MessagingResponse.new do |response|
+        response.message(body: "You have successfully subscribed to PainNavigator's reminder SMS messages!")
+      end
 
-    # Respond to Twilio with a simple message (optional)
-    twiml = Twilio::TwiML::MessagingResponse.new do |response|
-      response.message(body: "Hey!")
+    else
+      twiml = Twilio::TwiML::MessagingResponse.new do |response|
+        response.message(body: "Hello! This is PainNavigator's automated SMS messaging number. To get in touch with PainNavigator please message your Wellness Coach through the PainNavigator app! Otherwise to unsubscribe from these reminder text messages simply reply 'STOP' or to subscribe 'START'. Have a great day!")
+      end
     end
 
     render xml: twiml.to_s
