fb2c283ce5b58f61233237d04286f28324371704
Daniel Denton <danielmdenton@gmail.com>
2024-02-13 19:44:03 +0000

    typo fix

--- db/migrate/20240208185112_add_wellness_coach_reminded_to_users.rb
+++ db/migrate/20240208185112_add_wellness_coach_reminded_to_users.rb
@@ -1,5 +1,5 @@
 class AddWellnessCoachRemindedToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :wellness_coach_reminded, :boolean, defualt: true
+    add_column :users, :wellness_coach_reminded, :boolean, default: true
   end
 end
