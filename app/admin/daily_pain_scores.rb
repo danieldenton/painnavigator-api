df89e81c091f596128fe5e5372b4ffcd0920dde0
Daniel Denton <danielmdenton@gmail.com>
2024-03-09 22:09:07 +0000

    date formatting for daily pain score admin dashboard

--- app/admin/daily_pain_scores.rb
+++ app/admin/daily_pain_scores.rb
@@ -11,7 +11,6 @@ ActiveAdmin.register DailyPainScore do
     id_column
     column :user_id
     column :score
-    column :date_time_value
     column "date", sortable: :date_time_value do |date_time_value|
       Time.at(date_time_value / 1000).strftime("%Y-%m-%d")
     end
