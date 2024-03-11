15eee0dcecc57c7a2da3457ce208c0ee284aa3e6
Daniel Denton <danielmdenton@gmail.com>
2024-03-11 18:09:58 +0000

    updated daily_pain_score model to copy user_id to user_id_copy_for_admin

--- app/models/daily_pain_score.rb
+++ app/models/daily_pain_score.rb
@@ -1,3 +1,8 @@
 class DailyPainScore < ApplicationRecord
   belongs_to :user
+  before_create :copy_user_id_to_admin_copy
+
+  def copy_user_id_to_admin_copy
+    self.user_id_copy_for_admin = user_id
+  end
 end
