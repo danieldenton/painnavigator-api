b9029cb8a226a9a598af89f8f0e334e8e566cb44
Daniel Denton <danielmdenton@gmail.com>
2024-04-07 21:17:50 +0000

    updated movement_module destroy temporary

--- app/controllers/api/v2/movement_module_completions_controller.rb
+++ app/controllers/api/v2/movement_module_completions_controller.rb
@@ -30,14 +30,17 @@ module Api
       end
 
       def destroy
-        movement_modules = @user.movement_modules
+        movement_module = MovementModuleCompletion.find_by(id: params[:id])
+        movement_module.destroy
+        # movement_modules = @user.movement_modules
 
-        if movement_modules.destroy
+        # if movement_modules.destroy
+        if movement_module.destroy
         
           head :no_content
         else 
           render json: { error: user.errors.messages }, status: 422
-        end
+        # end
       end
 
       private
