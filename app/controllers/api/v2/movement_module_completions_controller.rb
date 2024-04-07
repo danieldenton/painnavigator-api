9b41d40783127983bb108b3de5657bf3cbd7592c
Daniel Denton <danielmdenton@gmail.com>
2024-04-07 22:04:50 +0000

    destroy restored

--- app/controllers/api/v2/movement_module_completions_controller.rb
+++ app/controllers/api/v2/movement_module_completions_controller.rb
@@ -29,27 +29,27 @@ module Api
         end
       end
 
-      # def destroy
-      #   movement_modules = @user.movement_modules
-
-      #   if movement_modules.destroy
-        
-      #     head :no_content
-      #   else 
-      #     render json: { error: user.errors.messages }, status: 422
-      #   end
-      # end
-
       def destroy
-        movement_module = MovementModuleCompletion.find_by(id: params[:id])
-      
-        if movement_module
-          movement_module.destroy
+        movement_modules = @user.movement_modules
+
+        if movement_modules.destroy
+        
           head :no_content
-        else
-          render json: { error: "Movement module not found" }, status: :not_found
+        else 
+          render json: { error: user.errors.messages }, status: 422
         end
       end
+
+      # def destroy
+      #   movement_module = MovementModuleCompletion.find_by(id: params[:id])
+      
+      #   if movement_module
+      #     movement_module.destroy
+      #     head :no_content
+      #   else
+      #     render json: { error: "Movement module not found" }, status: :not_found
+      #   end
+      # end
 
       private
 
