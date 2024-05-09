4979a83254d890d2a00e1e417a28b33186f1db87
Daniel Denton <danielmdenton@gmail.com>
2024-05-09 23:15:39 +0000

    starting pain scores works

--- app/controllers/provider_dashboard_controller.rb
+++ app/controllers/provider_dashboard_controller.rb
@@ -4,15 +4,12 @@ class ProviderDashboardController < ApplicationController
     
     @users = User.where(provider_id: @provider.id)
 
-    respond_to do |format|
-      format.html
-    end
+    @starting_pain_score_counts = {}
 
     starting_pain_scores = @users.map(&:starting_pain_score)
-    @starting_pain_score_counts = starting_pain_scores.group_by(&:itself).transform_values(&:count)
-
-    # @user_counts_by_month = @users.group_by_month(:created_at).count
-    # @earliest_month = @user_counts_by_month.keys.min
-    # @latest_month = @user_counts_by_month.keys.max
+    (1..10).each do |score| 
+      count = starting_pain_scores.count(score)
+      @starting_pain_score_counts[score] = count
+    end
   end
 end
