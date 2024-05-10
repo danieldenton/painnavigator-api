213d0fd2374b82712947a86ce16e752480e8523c
Daniel Denton <danielmdenton@gmail.com>
2024-05-10 16:28:28 +0000

    styling update

--- app/controllers/provider_dashboard_controller.rb
+++ app/controllers/provider_dashboard_controller.rb
@@ -11,5 +11,15 @@ class ProviderDashboardController < ApplicationController
       count = starting_pain_scores.count(score)
       @starting_pain_score_counts[score] = count
     end
+
+    user_counts_by_month = @users.group_by_month(:created_at).count
+
+    @cumulative_user_count = {}
+    cumulative_count = 0
+    user_counts_by_month.each do |month, count|
+      cumulative_count += count
+      @cumulative_user_count[month] = cumulative_count
+    end
+    
   end
 end
