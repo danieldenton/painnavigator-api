a409192dbe725350dee59004e9f0ddabb5015b0d
Daniel Denton <danielmdenton@gmail.com>
2024-05-11 00:06:51 +0000

    user all but done

--- app/controllers/provider_users_dashboard_controller.rb
+++ app/controllers/provider_users_dashboard_controller.rb
@@ -2,13 +2,20 @@ class ProviderUsersDashboardController < ApplicationController
   def user
     @user = User.find(params[:user_id])
 
+    @starting_pain_score = @user.starting_pain_score
+
+    @daily_pain_scores = DailyPainScore.where(user_id: @user.id)
+    @last_pain_score = @daily_pain_scores.any? ? @@daily_pain_scores.last.score : @starting_pain_score
+
     @number_of_logins = @user.dates_on_app.length
 
-    starting_pain_score = @user.starting_pain_score
-    daily_pain_scores = DailyPainScore.where(user_id: @user.id)
-    if daily_pain_scores.any?
-      last_daily_pain_score = daily_pain_scores.last.score
-    end
-    @pain_progress = { "satrting pain score": starting_pain_score, "current pain. score": last_daily_pain_score }
+    @education_module_completions = EducationModuleCompletion.where(user_id: @user.id).count 
+
+    movement_module_completions = MovementModuleCompletion.where(user_id: @user.id)
+    @movement_module_completions = movement_module_completions.any? ? movement_module_completions.last.module_id : 0
+
+
+    @smart_goal_count = SmartGoal.where(user_id: @user.id).count 
+
   end
 end
