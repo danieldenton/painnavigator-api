266a63debacc829a6900fe4be6e80328e190ee3e
Daniel Denton <danielmdenton@gmail.com>
2024-05-18 20:14:57 +0000

    config.assets.precompile += %w( chartkick/chartkick.js )

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
-  config.assets.precompile += %w( chartkick/chartkick.js )
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
